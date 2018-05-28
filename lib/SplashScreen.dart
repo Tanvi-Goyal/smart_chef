4f9ea88133c83ca82f7842f12b45ac93eece9d99
Tanvi-Goyal <goyaltanvi94@gmail.com>
2018-05-28 00:33:51 -0700

    Activities distributed

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'MyNavigator.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      Timer(Duration(seconds: 5),()=> MyNavigator.goToIntro(context));
+    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.red),
+          ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 50.0,
+                    child: Icon(
+                      Icons.fastfood,
+                      color: Colors.greenAccent,
+                      size: 50.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0)
+                    ),
+                    Text(
+                      "SmartChef" ,
+                       style: TextStyle(
+                         color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold),
+                    )
+                ],
+              ),
+            )
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                  ),
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              ),
+            )
+        ],
+        )
+        ],
+      ),
+    );
+  }
+}
